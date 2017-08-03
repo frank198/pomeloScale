7964d8db08e32cf61d8a73d03de4bf20ee520c73
460454039@qq.com <namei198>
2017-08-03 18:36:54 +0800

    bug 修改，添加移除 interval 的方法

--- test/util/utils.js
+++ test/util/utils.js
@@ -3,19 +3,19 @@
  */
 const Utils = require('../../lib/util/utils');
 
-describe("test Utils",() =>
+describe('test Utils', () =>
 {
-    it("LocalIps", done =>
-    {
-        const localIps = Utils.LocalIps;
-        console.info(localIps);
-        done();
-    });
+	it('LocalIps', done =>
+	{
+		const localIps = Utils.LocalIps;
+		console.info(localIps);
+		done();
+	});
 
-    it("IsLocal",done =>
-    {
-        const localIps = Utils.IsLocal('192.168.31.193');
-        console.info(localIps);
-        done();
-    })
+	it('IsLocal', done =>
+	{
+		const localIps = Utils.IsLocal('192.168.31.193');
+		console.info(localIps);
+		done();
+	});
 });
